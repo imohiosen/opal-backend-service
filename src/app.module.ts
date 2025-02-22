2580656a77df83f0ee60dabbaf8d6c506b717de7
Abraham Imohiosen <abrahamimohiosen@gmail.com>
2025-02-22 14:39:28 +0100

    Add health check endpoint and update dependencies for NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './health.controller';
 
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot()],
-  controllers: [AppController, CatsController],
+  imports: [UsersModule, ConfigModule.forRoot(), TerminusModule],
+  controllers: [AppController, CatsController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
